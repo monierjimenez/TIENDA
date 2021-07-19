2fe12b3d9ba2b7a29b70d4228f97a6b8ead1c7bc
monierjimenez <monierjimenez@gmail.com>
2021-07-18 20:39:14 -0600

    Primer comit

--- routes/web.php
+++ routes/web.php
@@ -5,7 +5,7 @@ use Illuminate\Support\Facades\Route;
     Route::get('/', function () {
         return view('welcome');
     });
-tienda
+
     Auth::routes();
     Route::get('/home', 'HomeController@index')->name('home');
 
